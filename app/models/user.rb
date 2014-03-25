cc3238f49d654e7a545b33898dfc8f9d88e96c7b
dennis eising <denniseising@hotmail.com>
2014-03-25 11:51:56 +0100

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+	  has_secure_password
+	  before_save { email.downcase! }
+	  before_create :create_remember_token
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+def feed
+  microposts
+end
+  
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'dfcc05a38e8d0bf56e5dd15e39d4030a047c7ac1aa7fa4a314ddc899d4c3f5df07c1ce12494e2e458ea17ba59d649bddfd30f5f4da324a98ed539c3afcdb6f66'
